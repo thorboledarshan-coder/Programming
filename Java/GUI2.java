432235073ce76eedd63a36a39f73669e4b256d83
Darshan <thorboledarshan@gmail.com>
2026-05-12 23:16:12 +0530

    Add files via upload

---
+++ Java/GUI2.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+class GUI2
+{
+    public static void main(String A[])
+    {
+        JFrame fobj=new Frame("Marvellous PPA");
+
+        fobj.setSize(400,300);
+
+        fobj.setVisible(true);
+    }
+}
